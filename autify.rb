d275bc91c48ba32c90b5f5faca5396f3561ba7f4
Victor Leong <victorleong@Victors-MacBook-Pro-16-Inch.local>
2023-02-11 06:29:09 +0900

    implement basic command line

--- autify.rb
+++ autify.rb
@@ -2,7 +2,8 @@
 
 # read file
 # handle the linux command stuff
-# dockerize it
+# dockerize it, make it executable with shell script https://stackoverflow.com/questions/61515104/how-to-convert-a-ruby-script-into-an-executable
+# refactor & handle possible errors
 
 # use nokogiri to get <a>, <img>, svg?
 # store in metadata
@@ -13,12 +14,16 @@
 
 # TODO: handle infinite scrolling pages, use selenium
 
+# ruby autify.rb https://autify.com/pricing https://www.google.com
+
 require 'open-uri'
 
-uri = URI.parse('https://autify.com/pricing')
+for arg in ARGV
+  uri = URI.parse(arg)
 
-open("#{uri.host.to_s}.html", "w+") do |file|
-  uri.open do |url|
-    file.write(url.read)
+  open("#{uri.host.to_s}.html", "w+") do |file|
+    uri.open do |url|
+      file.write(url.read)
+    end
   end
 end
